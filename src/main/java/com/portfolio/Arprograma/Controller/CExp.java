441f70196e507278f3ac01ce7fe0d6933ecacb1f
DasherSlade <nahuel4002@gmail.com>
2023-04-30 20:23:00 -0300

    Update url

--- src/main/java/com/portfolio/Arprograma/Controller/CExp.java
+++ src/main/java/com/portfolio/Arprograma/Controller/CExp.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://arprograma-front.web.app","http://localhost:4200"})
 public class CExp {
 
     @Autowired
